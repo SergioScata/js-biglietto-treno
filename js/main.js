8e6bd9c3bf493402a1b3e496fe1c5e762659d075
SergioScata <sergiopio395@gmail.com>
2023-11-13 14:14:14 +0100

    Started project

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let travellength= parseFloat(prompt("Inserisci quanti km vuoi percorrere"));
+let age= parseInt(prompt("Inserisci la tua età"));
+let ticketprice= travellength * 0,21;
+
+if (age>=65) {
+    ticketprice
+}
+else if (age<=20) {
+    
+}
