20562654934297405ccd62af91301819402dca15
SergioScata <sergiopio395@gmail.com>
2023-11-13 14:57:42 +0100

    Work done

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
+//Richiediamo all'utente i dati di cui abbiamo bisogno
 let travellength= parseFloat(prompt("Inserisci quanti km vuoi percorrere"));
 let age= parseInt(prompt("Inserisci la tua età"));
 let ticketprice= (travellength * 0.21);
 let finalprice = ticketprice.toFixed(2);
 
+
+//Sezione degli if
 if (age>=65) {
    ticketprice= (ticketprice * 0.6);
 }
@@ -13,6 +16,7 @@ else {
     ticketprice= ticketprice;
 }
 
+//riportiamo il messaggio sia nella console che in Html
 console.log ("Il prezzo del tuo biglietto è:" + finalprice);
 
 document.getElementById("utent-price").innerHTML="Il prezzo del tuo biglietto è €" + finalprice;
