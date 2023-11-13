c27a8c798a1be8e29f36228ec7ea081215ecf546
SergioScata <sergiopio395@gmail.com>
2023-11-13 14:43:26 +0100

    Updated project

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
 let travellength= parseFloat(prompt("Inserisci quanti km vuoi percorrere"));
 let age= parseInt(prompt("Inserisci la tua età"));
-let ticketprice= travellength * 0,21;
+let ticketprice= (travellength * 0.21);
+let finalprice = ticketprice.toFixed(2);
 
 if (age>=65) {
-    ticketprice
+   ticketprice= (ticketprice * 0.6);
 }
-else if (age<=20) {
-    
+else if (age<18) {
+    ticketprice= (ticketprice * 0.8);
 }
+else {
+    ticketprice= ticketprice;
+}
+
+console.log ("Il prezzo del tuo biglietto è:" + finalprice);
+
+document.getElementById("utent-price").innerHTML="Il prezzo del tuo biglietto è €" + finalprice;
